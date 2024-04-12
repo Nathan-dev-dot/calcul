b4486ce3147fc52fe201f5a3e8784bffb9b7b46d
Nathan-dev-dot <maerna1@gmail.com>
2024-04-12 14:09:51 +0200

    display

--- src/main/java/Calcul.java
+++ src/main/java/Calcul.java
@@ -11,27 +11,47 @@ public class Calcul {
 
     protected Integer add(List<Integer> numbers) {
         AtomicReference<Integer> total = new AtomicReference<>(0);
+        AtomicInteger start = new AtomicInteger();
+
         numbers.forEach((number) -> {
             total.updateAndGet(v -> v + number);
+            start.updateAndGet(v -> v + 1);
+            convert(number, total, start, "+", numbers.size() );
         });
+
         return total.get();
+    }
+
+    private void convert(Integer number, AtomicReference total, AtomicInteger start, String args, Integer listSize){
+        if(start.get() != 1){
+            print.print(args + number.toString() + " (=" + total + ")");
+        } else{
+            print.print(number.toString());
+        }
+        if (start.get() ==  listSize){
+            print.print("_______");
+        }
     }
 
     protected Integer time(List<Integer> numbers) {
         AtomicReference<Integer> total = new AtomicReference<>(1);
+        AtomicInteger start = new AtomicInteger();
         numbers.forEach((number) -> {
-            total.set(total.get() * number);
+            total.updateAndGet(v -> v * number);
+            start.addAndGet(1);
+            convert(number, total, start, "*", numbers.size());
         });
+
         return total.get();
     }
 
     public void process(List<Integer> numbers, String arg) throws ArithmeticException {
         switch (arg){
             case "+":
-                print.print("total = " + this.add(numbers).toString() + "(addition)");
+                print.print("total = " + this.add(numbers).toString() + " (addition)");
                 break;
             case "*":
-                print.print("total = " + this.add(numbers).toString() + "(multiplication)");
+                print.print("total = " + this.time(numbers).toString() + " (multiplication)" );
                 break;
             default:
                 throw new ArithmeticException("Not permitted");
--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,4 +1,7 @@
+import java.util.List;
+
 public class Print implements PrintInterface {
+    private Calcul calcul = new Calcul(this);
     public void print(String message) {
         System.out.println(message);
     }
